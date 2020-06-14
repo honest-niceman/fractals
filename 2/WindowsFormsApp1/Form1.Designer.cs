d78be37305cea72f170695533e9c9c64696da98a
honest-niceman <56090398+honest-niceman@users.noreply.github.com>
2020-06-14 18:49:33 +0400

    Add files via upload

---
+++ 2/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.TaskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.Location = new System.Drawing.Point(150, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1188, 738);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Wheat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(143, 946);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Построить фрактал";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(174, 831);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Правило: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(175, 800);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "0-ой шаг: ";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(577, 802);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(543, 274);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(174, 892);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Шаг №:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(357, 895);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(197, 26);
+            this.textBox1.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Wheat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button2.Location = new System.Drawing.Point(357, 946);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 57);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Wheat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button3.Location = new System.Drawing.Point(143, 1019);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(197, 57);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Шаг назад";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Wheat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button4.Location = new System.Drawing.Point(357, 1019);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(197, 57);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Шаг вперёд";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(357, 831);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(197, 26);
+            this.textBox2.TabIndex = 12;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(357, 800);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(197, 26);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(175, 863);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 29);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Угол:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(357, 863);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(197, 26);
+            this.textBox4.TabIndex = 15;
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TaskToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1552, 33);
+            this.menuStrip1.TabIndex = 16;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // TaskToolStripMenuItem
+            // 
+            this.TaskToolStripMenuItem.Name = "TaskToolStripMenuItem";
+            this.TaskToolStripMenuItem.Size = new System.Drawing.Size(132, 29);
+            this.TaskToolStripMenuItem.Text = "Мой вариант";
+            this.TaskToolStripMenuItem.Click += new System.EventHandler(this.мойВариантToolStripMenuItem_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Русский",
+            "English",
+            "Français "});
+            this.comboBox1.Location = new System.Drawing.Point(1126, 804);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(211, 28);
+            this.comboBox1.TabIndex = 20;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1552, 1162);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа №2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem TaskToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ 2/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Resources;
+using System.Globalization;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0; 
+        }
+
+        public void Drawing(String rule, Graphics e, Pen locpen, float alfa)
+        {
+            int k = 7;
+            float x0 = pictureBox1.Width / 4f;
+            float y0 = pictureBox1.Height / 3f;
+            float x1 = x0;
+            float y1 = y0;
+            float alfa1 = 0;          
+            Stack<float> stack = new Stack<float>();
+            char[] Mas_Rule = rule.ToCharArray();
+            for (int i = 0; i < Mas_Rule.Length; i++)
+            {
+                switch (Mas_Rule[i])
+                {
+                    case 'F':
+                        {
+                            x1 += (float)(2 * k * Math.Cos((alfa1 * Math.PI) / 180));
+                            y1 += (float)(2 * k * Math.Sin((alfa1 * Math.PI) / 180));
+                            e.DrawLine(locpen, x0, y0, x1, y1);
+                            x0 = x1;
+                            y0 = y1;
+                            break;
+                        }
+                    case '+':
+                        {
+                            alfa1 += alfa;
+                            break;
+                        }
+                    case '-':
+                        {
+                            alfa1 -= alfa;
+                            break;
+                        }
+                    case '[':
+                        {
+                            stack.Push(y1);
+                            stack.Push(x1);
+                            stack.Push(alfa1);
+                            break;
+                        }
+                    case ']':
+                        {
+                            alfa1 = stack.Pop();
+                            x1 = stack.Pop();
+                            y1 = stack.Pop();
+                            x0 = x1;
+                            y0 = y1;
+                            break;
+                        }
+                }
+            }
+        }
+
+        public string Printing(string Axiom, string rule, int step)
+        {
+            string LastFormula = Axiom;
+            if (step == 0)
+            {
+                char[] tmp = LastFormula.ToCharArray();
+                LastFormula = Axiom;
+            }
+            else
+            {
+                for (int j = 0; j < step; j++)
+                {
+                    int k = 0;
+                    char[] tmp = LastFormula.ToCharArray();
+                    for (int i = 0; i < tmp.Length; i++)
+                    {
+                        if (tmp[i] == 'F')
+                        {
+                            LastFormula = LastFormula.Remove(k, 1);
+                            LastFormula = LastFormula.Insert(k, rule);
+                            k += rule.Length;
+                            continue;
+                        }
+                        k++;
+                    }
+                }
+            }
+            return LastFormula;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pen pen1 = new Pen(Color.Black, 1);
+            Graphics gr = pictureBox1.CreateGraphics();
+            if (int.TryParse(textBox1.Text, out int i) && (i < 11 && i > -1))
+            {
+
+                richTextBox1.Text = Printing(textBox3.Text, textBox2.Text, i);
+                gr.Clear(Color.White);
+                Drawing(richTextBox1.Text, gr, pen1, float.Parse(textBox4.Text));
+            }
+            else
+            {
+                textBox1.Text = "";
+                textBox1.Focus();
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            pictureBox1.Image = null;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Pen pen1 = new Pen(Color.Black, 1);
+            Graphics gr = pictureBox1.CreateGraphics();
+            if (int.TryParse(textBox1.Text, out int i) && (i < 12 && i > 0))
+            {
+
+                richTextBox1.Text = Printing(textBox3.Text, textBox2.Text, i-1);
+                gr.Clear(Color.White);
+                Drawing(richTextBox1.Text, gr, pen1, float.Parse(textBox4.Text));
+                i -= 1;
+                textBox1.Text = i.ToString();
+            }
+            else
+            {
+                textBox1.Text = "";
+                textBox1.Focus();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Pen pen1 = new Pen(Color.Black, 1);
+            Graphics gr = pictureBox1.CreateGraphics();
+            if (int.TryParse(textBox1.Text, out int i) && (i < 10 && i > -2))
+            {
+
+                richTextBox1.Text = Printing(textBox3.Text, textBox2.Text, i + 1);
+                gr.Clear(Color.White);
+                Drawing(richTextBox1.Text, gr, pen1, float.Parse(textBox4.Text));
+                i += 1;
+                textBox1.Text = i.ToString();
+            }
+            else
+            {
+                textBox1.Text = "";
+                textBox1.Focus();
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (Char)Keys.Back)
+            {
+                return;
+            }
+            if (e.KeyChar == 'F')
+            {
+                return;
+            }
+            if(e.KeyChar == '-')
+            {
+                return;
+            }
+            if(e.KeyChar == '+')
+            {
+                return;
+            }
+            if (e.KeyChar == '[')
+            {
+                return;
+            }
+            if(e.KeyChar == ']')
+            {
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (Char)Keys.Back)
+            {
+                return;
+            }
+            if (e.KeyChar == 'F')
+            {
+                return;
+            }
+            if (e.KeyChar == '-')
+            {
+                return;
+            }
+            if (e.KeyChar == '+')
+            {
+                return;
+            }
+            if (e.KeyChar == '[')
+            {
+                return;
+            }
+            if (e.KeyChar == ']')
+            {
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if((e.KeyChar >= '0') && (e.KeyChar <= '9'))
+            {
+                return;
+            }
+            if (e.KeyChar == (Char)Keys.Back)
+            {
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void мойВариантToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = "F+F+F+F";
+            textBox2.Text = "FF+F+F+F+F+F-F";
+            textBox4.Text = "90";
+            textBox1.Text = "1";
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                CultureInfo ci = new CultureInfo("ru-RU");
+                Assembly a = Assembly.Load("WindowsFormsApp1");
+                ResourceManager rm = new ResourceManager("WindowsFormsApp1.Lang-Ru", a);
+                label1.Text = rm.GetString("rule", ci);
+                label2.Text = rm.GetString("step0", ci);
+                label3.Text = rm.GetString("step", ci);
+                label4.Text = rm.GetString("angle", ci);
+                button1.Text = rm.GetString("create", ci);
+                button2.Text = rm.GetString("clear", ci);
+                button3.Text = rm.GetString("back", ci);
+                button4.Text = rm.GetString("forward", ci);
+                TaskToolStripMenuItem.Text = rm.GetString("task", ci);
+                menuStrip1.Text = rm.GetString("labrab", ci);
+            }
+            if(comboBox1.SelectedIndex == 1)
+            {
+                CultureInfo ci = new CultureInfo("en-US");
+                Assembly a = Assembly.Load("WindowsFormsApp1");
+                ResourceManager rm = new ResourceManager("WindowsFormsApp1.Lang-En", a);
+                label1.Text = rm.GetString("rule", ci);
+                label2.Text = rm.GetString("step0", ci);
+                label3.Text = rm.GetString("step", ci);
+                label4.Text = rm.GetString("angle", ci);
+                button1.Text = rm.GetString("create", ci);
+                button2.Text = rm.GetString("clear", ci);
+                button3.Text = rm.GetString("back", ci);
+                button4.Text = rm.GetString("forward", ci);
+                TaskToolStripMenuItem.Text = rm.GetString("task", ci);
+                menuStrip1.Text = rm.GetString("labrab", ci);
+            }
+            if(comboBox1.SelectedIndex == 2)
+            {
+
+                CultureInfo ci = new CultureInfo("fr-FR");
+                Assembly a = Assembly.Load("WindowsFormsApp1");
+                ResourceManager rm = new ResourceManager("WindowsFormsApp1.Lang-Fr", a);
+                label1.Text = rm.GetString("rule", ci);
+                label2.Text = rm.GetString("step0", ci);
+                label3.Text = rm.GetString("step", ci);
+                label4.Text = rm.GetString("angle", ci);
+                button1.Text = rm.GetString("create", ci);
+                button2.Text = rm.GetString("clear", ci);
+                button3.Text = rm.GetString("back", ci);
+                button4.Text = rm.GetString("forward", ci);
+                TaskToolStripMenuItem.Text = rm.GetString("task", ci);
+                menuStrip1.Text = rm.GetString("labrab", ci);
+            }
+        }
+    }
+}
